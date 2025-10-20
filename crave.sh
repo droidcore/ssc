e21de57b589efc694a70f37db5a726d5d6f0b6b0
droidcore <agusmahardika99@gmail.com>
2025-10-21 04:58:38 +0700

    Enhance project setup and build script
    Refactor script to improve project initialization and build process.

--- crave.sh
+++ crave.sh
@@ -1,40 +1,59 @@
 #!/bin/bash
 
-# ================================
-# Clean old manifests
-# ================================
-rm -rf .repo/local_manifests/
+set -e
+#Credit to Meghthedev for the initial script 
 
-# ================================
-# Initialize Lunaris-AOSP repo
-# ================================
-echo ">>> Initializing RisingOS repository"
-repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
+export PROJECTFOLDER="LOS"
+export PROJECTID="93"
+export REPO_INIT="repo init -u https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs --depth=1"
+export BUILD_DIFFERENT_ROM="repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs" # Change this if you'd like to build something else
 
-# ================================
-# Clone device/vendor/kernel trees
-# ================================
-echo ">>> Cloning Device Trees"
-git clone https://github.com/droidcore/device_xiaomi_peridot.git -b lineage-23.0 device/xiaomi/peridot
+# Destroy Old Clones
+if (grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"')) || [ "${DCDEVSPACE}" == "1" ]; then   
+   crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
+else
+   rm -rf $PROJECTFOLDER || true
+fi
 
-# ================================
-# Sync remaining sources
-# ================================
-echo ">>> Syncing repo"
-/opt/crave/resync.sh
+# Create New clone
+if [ "${DCDEVSPACE}" == "1" ]; then
+   crave clone create --projectID $PROJECTID /crave-devspaces/$PROJECTFOLDER || echo "Crave clone create failed!"
+   cd /crave-devspaces/$PROJECTFOLDER
+else
+   mkdir $PROJECTFOLDER
+   cd $PROJECTFOLDER
+   echo "Running $REPO_INIT"
+   $REPO_INIT
+fi
 
-# ================================
-# Setup build environment
-# ================================
-. b*/env*
-export BUILD_USERNAME=BLU
-export BUILD_HOSTNAME=crave
-export TZ=Asia/Jakarta
+# Run inside foss.crave.io devspace
+# Remove existing local_manifests
+crave run --no-patch -- "rm -rf .repo/local_manifests && \
 
-# ================================
-# Start build
-# ================================
-echo ">>> Starting RisingOS Build"
-lunch lineage_peridot-bp2a-user
-make installclean
-m lunaris
+# Init Manifest
+$BUILD_DIFFERENT_ROM && \
+
+# Clone local_manifests repository
+git clone https://github.com/droidcore/local_manifest.git --depth 1 -b lineages-23.0 .repo/local_manifests && \
+
+ # Sync the repositories
+ /opt/crave/resync.sh && \ 
+
+# Set up build environment
+source build/envsetup.sh && \
+
+# Lunch configuration
+lunch lineage_peridot-user && \
+
+# Clean build (installclean)
+make installclean && \
+
+# Build the ROM
+m lunaris"
+
+# Clean up
+if grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"') || [ "${DCDEVSPACE}" == "1" ]; then
+  crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
+else  
+  rm -rf $PROJECTFOLDER || true
+fi
