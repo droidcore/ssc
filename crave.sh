dc946701d1cdf7cf67f9f87505f60ba857b5b6ff
droidcore <agusmahardika99@gmail.com>
2025-12-01 12:46:36 +0700

    Add files via upload

--- crave.sh
+++ crave.sh
@@ -1,41 +1,5 @@
 #!/bin/bash
-set -e
 
-# ================================
-# Project Configuration
-# ================================
-export PROJECTFOLDER="aosp"
-export PROJECTID="35"
-export REPO_INIT="repo init -u https://android.googlesource.com/platform/manifest"
-export BUILD_DIFFERENT_ROM="repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs" # Change this if you'd like to build something else
-
-# ================================
-# Destroy Old Clones
-# ================================
-echo ">>> Cleaning old clone"
-if (grep -q "$PROJECTFOLDER" <(crave clone list --json | jq -r '.clones[]."Cloned At"')) || [ "${DCDEVSPACE}" == "1" ]; then
-  crave clone destroy -y /crave-devspaces/$PROJECTFOLDER || echo "Error removing $PROJECTFOLDER"
-else
-  rm -rf $PROJECTFOLDER || true
-fi
-
-# ================================
-# Create New Clone
-# ================================
-echo ">>> Creating new clone"
-if [ "${DCDEVSPACE}" == "1" ]; then
-  crave clone create --projectID $PROJECTID /crave-devspaces/$PROJECTFOLDER || echo "Crave clone create failed!"
-  cd /crave-devspaces/$PROJECTFOLDER
-else
-  mkdir $PROJECTFOLDER && cd $PROJECTFOLDER
-  echo "Running $REPO_INIT"
-  $REPO_INIT
-fi
-
-# ================================
-# Run inside Crave devspace
-# ================================
-crave run --no-patch -- "
   # ================================
   # Clean old manifests
   # ================================
@@ -43,12 +7,6 @@ crave run --no-patch -- "
   rm -rf device/xiaomi/peridot
   rm -rf out/target/product/peridot
   
-  # ================================
-  # Initialize Evox repo
-  # ================================
-  echo '>>> Initializing Lunaris-AOSP repo'
-  $BUILD_DIFFERENT_ROM
-
   # ================================
   # Clone local manifests
   # ================================
