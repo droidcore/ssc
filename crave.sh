448f334ef902db63592f6114e8b28d92fb8ef7e3
droidcore <agusmahardika99@gmail.com>
2025-12-29 06:55:47 +0700

    Update crave.sh

--- crave.sh
+++ crave.sh
@@ -7,6 +7,8 @@ set -e
 export PROJECTFOLDER="LOS"
 export PROJECTID="93"
 export REPO_INIT="https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs --depth=1"
+export BUILD_DIFFERENT_ROM="repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs" # Change this if you'd like to build something else
+
 # ================================
 # Destroy Old Clones
 # ================================
