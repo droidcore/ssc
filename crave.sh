1a8d890a96c583c67ff188e62416c62da3e11a32
droidcore <agusmahardika99@gmail.com>
2025-12-28 17:33:23 +0700

    Add files via upload

--- crave.sh
+++ crave.sh
@@ -7,7 +7,7 @@ set -e
 export PROJECTFOLDER="LOS"
 export PROJECTID="93"
 export REPO_INIT="https://github.com/accupara/los22.git -b lineage-22.1 --git-lfs --depth=1"
-export BUILD_DIFFERENT_ROM="repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs"
+export BUILD_DIFFERENT_ROM="repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.1 --git-lfs"
 # ================================
 # Destroy Old Clones
 # ================================
@@ -63,7 +63,7 @@ crave run --no-patch -- "
   # ================================
   # Setup build environment
   # ================================
-  . b*/env*
+  . build/envsetup.sh
   export BUILD_USERNAME=BLU
   export BUILD_HOSTNAME=crave
   export TZ=Asia/Jakarta
@@ -72,7 +72,7 @@ crave run --no-patch -- "
   # Build
   # ================================
   echo '>>> Starting build'
-  lunch lineage_peridot-bp2a-user
+  axion peridot user gms core
   make installclean
-  m lunaris
+  ax -br
 "
